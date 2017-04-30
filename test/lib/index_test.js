eb0f00284ed5d6f104cf43cbc18aee2b786040a1
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-30 14:00:01 -0600

    Add merge sort

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -1,10 +1,18 @@
 const expect = require('expect');
-const bubbleSort = require('../../lib/index');
+const bubbleSort = require('../../lib/index').bubbleSort;
+const mergeSort = require('../../lib/index').mergeSort;
 const _ = require('lodash');
 
 describe('bubble sort', function() {
   it('sorts the array from lowest to highest', function() {
     expect(bubbleSort(["d", "b", "a", "c"])).toEqual(["a", "b", "c", "d"]);
     expect(bubbleSort(_.shuffle(_.range(100)))).toEqual(_.range(100));
+  })
+})
+
+describe('merge sort', function() {
+  it('sorts the array from lowest to highest', function() {
+    expect(mergeSort(["d", "b", "a", "c"])).toEqual(["a", "b", "c", "d"]);
+    expect(mergeSort(_.shuffle(_.range(100)))).toEqual(_.range(100));
   })
 })
